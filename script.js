b2ea8dfcc15b349773e9abcef74c56915b7c1003
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-14 17:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let count = document.getElementById('counter');
+let increase = document.getElementById('incrementBtn');
+let score = 0;
+count.innerHTML = score;
+increase.addEventListener("click",()=>{
+	score++;
+	count.innerHTML = score;
+});
